e54b80614040189f1e1e0cea631eafe9e40c57b4
mikron <mikron@thor.gateway.sonic.net>
2018-07-07 13:40:31 -0700

    repo clean up

--- my_linux_academy/my_python_class/content-python-for-sys-admins/pgbackup/src/pgbackup/storage.py
+++
@@ -1,7 +0,0 @@
-def local(infile, outfile):
-    outfile.write(infile.read())
-    outfile.close()
-    infile.close()
-
-def s3(client, infile, bucket, file_name):
-    client.upload_fileobj(infile, bucket, file_name)
