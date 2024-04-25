660048294923ab7de218a06050af828c35b0e428
mikronixx <mikron@frieswiththat.org>
2024-04-25 04:08:00 +0000

    includes session and get ec2 instances example

--- useful_python/aws_boto3_examples.py
+++ useful_python/aws_boto3_examples.py
@@ -3,13 +3,25 @@ import boto3
 
 #basic boto3 auth using local aws credentials 
 def boto3session():
-    session=boto3.Session(
-    region_name="us-west-2",
-    profile_name="default")
-    return session
-print(session)
-    
-#use it in an ec2 client to describe images 
+    boto3session=boto3.Session(
+    region_name="us-west-2")
+    return boto3session
+boto3session=boto3session()
 
-def findimages(session):
-    page=session.client(ec2).getpaginator('describe_images').paginate.build_full_result()
+#create a client that returns a paginated object, which is easier to parse than a dict. 
+#boto3 pagination, and why it is cool.  
+#aws ec2 describe-instances <args, incl --instance-id> are your friends here
+
+def ec2instances(boto3session):
+    ec2instances=[]
+    pagination_filters = [{
+        'Name': 'instance-state-name',
+        'Values': ['running']}]
+    page = boto3session.client('ec2').get_paginator('describe_instances').paginate(Filters=pagination_filters).build_full_result()
+    for reservation in page['Reservations']:
+        for instance in reservation['Instances']:
+            ec2instances.append([instance['InstanceId'],instance['PublicIpAddress'], instance['PrivateIpAddress']])  
+    return ec2instances
+
+print(ec2instances(boto3session))
+
